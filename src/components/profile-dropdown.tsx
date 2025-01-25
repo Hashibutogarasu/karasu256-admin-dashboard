6e688eb928e12420bc06fa124913979ec63aa506
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-01-25 23:12:54 +0900

    feat: update AvatarImage source to use user profile picture and nickname

--- src/components/profile-dropdown.tsx
+++ src/components/profile-dropdown.tsx
@@ -21,7 +21,7 @@ export function ProfileDropdown() {
       <DropdownMenuTrigger asChild>
         <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
           <Avatar className='h-8 w-8'>
-            <AvatarImage src='/avatars/01.png' alt='@shadcn' />
+            <AvatarImage src={user?.picture} alt={user?.nickname} />
             <AvatarFallback>{`${user?.nickname![0]}${user?.nickname![1].toUpperCase()}`}</AvatarFallback>
           </Avatar>
         </Button>
