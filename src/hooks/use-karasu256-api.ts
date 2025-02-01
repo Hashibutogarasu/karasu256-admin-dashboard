14c8f232b64faaa20124adca2e9aa648ad59936b
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-02-01 16:56:22 +0900

    fix: update API base URL to use VITE_BASE_URL in use-karasu256-api hook

--- src/hooks/use-karasu256-api.ts
+++ src/hooks/use-karasu256-api.ts
@@ -3,7 +3,7 @@ import { APIClient } from '@karasu-lab/karasu256-api-client'
 export function useKarasu256API() {
   const api = new APIClient();
 
-  api.characters.httpRequest.config.BASE = `${import.meta.env.BASE_URL}/api`
+  api.characters.httpRequest.config.BASE = `${import.meta.env.VITE_BASE_URL}/api`
 
   if (import.meta.env.DEV) {
     api.characters.httpRequest.config.HEADERS = {
