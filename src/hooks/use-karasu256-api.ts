8b30dcdb3067e726143e544d141019235cf41ff6
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-02-01 17:07:50 +0900

    refactor: rename useKarasu256API hook to useAPIWithCredentials and update API initialization

--- src/hooks/use-karasu256-api.ts
+++ src/hooks/use-karasu256-api.ts
@@ -1,10 +1,18 @@
 import { APIClient } from '@karasu-lab/karasu256-api-client'
 
-export function useKarasu256API() {
+export function useAPIWithCredentials() {
   const api = new APIClient({
     HEADERS: {
       'Authorization': import.meta.env.DEV ? `Bearer ${import.meta.env.VITE_API_BEARER}` : `Bearer ${sessionStorage.getItem('accessToken')}`
     },
+    BASE: `${import.meta.env.VITE_BASE_URL}/api`
+  });
+
+  return api;
+}
+
+export function useKarasu256API() {
+  const api = new APIClient({
     BASE: `${import.meta.env.VITE_BASE_URL}/api`
   });
 
