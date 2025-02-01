5c4414f33c373affb9ff45a1e915ceceb57bbb8d
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-02-01 16:30:28 +0900

    feat: update API configuration and add proxy settings in Vite config

--- src/hooks/use-karasu256-api.ts
+++ src/hooks/use-karasu256-api.ts
@@ -3,11 +3,12 @@ import { APIClient } from '@karasu-lab/karasu256-api-client'
 export function useKarasu256API() {
   const api = new APIClient();
 
+  api.characters.httpRequest.config.BASE = `${import.meta.env.BASE_URL}/api`
+
   if (import.meta.env.DEV) {
     api.characters.httpRequest.config.HEADERS = {
       Authorization: `Bearer ${import.meta.env.VITE_API_BEARER}`
     }
-    api.characters.httpRequest.config.BASE = import.meta.env.VITE_API_HOST
   }
   else {
     api.characters.httpRequest.config.HEADERS = {
--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,37 @@
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default ({ mode }: { mode: string }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-      // fix loading all icon chunks in dev mode
-      // https://github.com/tabler/tabler-icons/issues/1233
-      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+
+  return defineConfig({
+    plugins: [react(), TanStackRouterVite()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+
+        // fix loading all icon chunks in dev mode
+        // https://github.com/tabler/tabler-icons/issues/1233
+        '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+      },
     },
-  },
-  define: {
-    global: 'window',
-  },
-  server: {
-
-    cors: {
-      //https://karasu-lab-storage.dc4a5b8ffc2d999e39e9f9509bb4f9af.r2.cloudflarestorage.com/karasu-lab-storage/
-      origin: 'https://karasu-lab-storage.dc4a5b8ffc2d999e39e9f9509bb4f9af.r2.cloudflarestorage.com',
-      allowedHeaders: 'Authorization, Content-Type, Access-Control-Allow-Origin',
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    }
+    define: {
+      global: 'window',
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_HOST,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        }
+      }
+    },
   }
-})
+  )
+}
